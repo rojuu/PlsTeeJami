32ef1a2d4537f9faf95eedbb04d527377b8a5a3b
Kuusipuro Kasperi TTK16SP <kasperikuusipuro@kamk.fi>
2017-05-06 16:44:59 +0300

    Gamemanager + fish prefabs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GM;
+
+    public List<GameObject> fishPrefabs;
+    
+    private void Awake()
+    {
+        if (GM == null)
+            GM = this;
+        else if (GM != this)
+            Destroy(gameObject);
+    }
+
+
+}
