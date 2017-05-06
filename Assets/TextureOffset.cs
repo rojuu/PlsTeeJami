1256e27eb5378f2c88c7e0ad8e1fa5d1e0634c1d
rojuu <roju269@gmail.com>
2017-05-06 23:43:47 +0300

    Testing water shader

---
+++ Assets/TextureOffset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffset : MonoBehaviour {
+
+    public float scrollSpeed = 1;
+
+    new MeshRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update ()
+    {
+        renderer.material.mainTextureOffset = new Vector2((Time.time * scrollSpeed) % 1, 0);
+	}
+}
