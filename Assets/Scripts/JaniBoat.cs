21ce399daec3afc5bdcb750650a376d0a7e03154
Leinonen Jani TTK16SP <janileinonen1@kamk.fi>
2017-05-06 13:45:34 +0300

    canvas started etc

---
+++ Assets/Scripts/JaniBoat.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaniBoat : MonoBehaviour
+{
+    private bool mouseOver;
+    private MonitorScreen monitorScreen;
+    private Boat boat; // zoomed boat
+
+    public GameObject screenPermit;
+
+    void Start ()
+    {
+        monitorScreen = screenPermit.GetComponent<MonitorScreen>();
+        boat = GameObject.Find("BigBoat").GetComponent<Boat>();
+    }
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0) && mouseOver)
+        {
+            monitorScreen.SetPermitObjects(boat.permit);
+            if (!screenPermit.activeInHierarchy)
+            {
+                screenPermit.SetActive(true);
+            }
+        }
+	}
+
+    void OnMouseEnter()
+    {
+        mouseOver = true;
+    }
+    void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+}
---
+++ Assets/Scripts/MonitorScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonitorScreen : MonoBehaviour {
+    
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void SetPermitObjects(Permit boatPermit)
+    {
+        Text[] permitTexts = GetComponentsInChildren<Text>();
+        permitTexts[0].text = boatPermit.registerNumber;
+        permitTexts[1].text = boatPermit.expirationDate.ToString();
+        permitTexts[2].text = boatPermit.owner;
+        for (int i = 0; i < boatPermit.allowedFishes.Count; i++)
+        {
+            permitTexts[3 + i].text = boatPermit.allowedFishes[i].name;
+        }
+    }
+}
